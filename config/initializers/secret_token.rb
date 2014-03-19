eb9d3bf761afa0e11afd7bca3d069657e67b49b9
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2014-03-19 11:52:54 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RapGenius::Application.config.secret_key_base = '1fa942281d58726d0181488a8c3381726c7a276957938754ce58630368a6bff75afa52a161b047a0caa469629604f8d72763171749a4b11e9f06eaa5228b99e7'
