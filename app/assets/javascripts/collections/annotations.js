b5cffc39d9d14d501d9a5cd5cf0ae27b2b8ff97f
Ryan O'Donnell <ryan.jp.odonnell@gmail.com>
2014-03-29 14:03:23 -0700

    Revert to 9ff88a0

--- app/assets/javascripts/collections/annotations.js
+++ app/assets/javascripts/collections/annotations.js
@@ -2,7 +2,7 @@ RapGenius.Collections.Annotations = Backbone.Collection.extend({
   model: RapGenius.Models.Annotation,
   url: 'api/annotations',
   comparator: function(model) {
-    return model.escape("start_index");
+    return model.get("start_index");
   },
   intialize: function(models, options) {
     this.track = options.track;
--- app/assets/javascripts/views/tracks/tracks_new.js
+++ app/assets/javascripts/views/tracks/tracks_new.js
@@ -13,7 +13,7 @@ RapGenius.Views.TracksNew = Backbone.View.extend({
   render: function () {  
     var availableArtists = [];
     for (var i = 0; i < this.artists.length; i++) {
-      availableArtists.push(this.artists.models[i].escape('artistname'));
+      availableArtists.push(this.artists.models[i].get('artistname'));
     }
     
     var renderedContent = this.template();
@@ -37,7 +37,7 @@ RapGenius.Views.TracksNew = Backbone.View.extend({
     var foundArtist = false;
     
     for (var i = 0; i < this.artists.length; i++) {
-      if (artistName === this.artists.models[i].escape('artistname')) {
+      if (artistName === this.artists.models[i].get('artistname')) {
         foundArtist = true;
       }
     }
